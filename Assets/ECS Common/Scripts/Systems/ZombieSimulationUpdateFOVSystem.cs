27e0e5c80744bef2dd2208229c555192429d3be6
Pierre Michel <pierre.michel@unity3d.com>
2023-10-12 14:19:56 +0200

    Job-ified FOV system

--- Assets/ECS Common/Scripts/Systems/ZombieSimulationUpdateFOVSystem.cs
+++ Assets/ECS Common/Scripts/Systems/ZombieSimulationUpdateFOVSystem.cs
@@ -26,59 +26,78 @@ public partial struct ZombieSimulationUpdateFOVSystem : ISystem
 
 		var settings = SystemAPI.GetSingleton<ZombieSimulationSettings>();
 		float fovAngleCos = math.cos(math.radians(settings.FOVAngle));
-
-		NativeList<SpatialHashEntityData> spatialHashResult = new(Allocator.Temp);
 		var spatialHash = SystemAPI.GetSingletonRW<ZombieSimulationSpatialHashData>();
+		
 
-		foreach (var (fovResults, movement, localTransform, entity) in
-				SystemAPI.Query<RefRW<ZombieSimulationAgentFOVResults>, ZombieSimulationMovement, LocalTransform>().WithEntityAccess())
+		var job = new UpdateFOVJob
 		{
-			spatialHashResult.Clear();
-			fovResults.ValueRW.CanSeeZombie = false;
-			fovResults.ValueRW.CanSeeHealthyHuman = false;
-			fovResults.ValueRW.NearestVisibleHealthyEntity = Entity.Null;
+			Settings = settings,
+			SpatialHash = spatialHash.ValueRO,
+			FovAngleCos = fovAngleCos
+		};
+
+		var jobHandle = job.ScheduleParallel(state.Dependency);
+		state.Dependency = jobHandle;
+		jobHandle.Complete();
+
+		m_Marker.End();
+	}
+
+	[BurstCompile]
+	public partial struct UpdateFOVJob : IJobEntity
+	{
+		[ReadOnly] public float FovAngleCos;
+		[ReadOnly] public ZombieSimulationSettings Settings;
+		[ReadOnly] public ZombieSimulationSpatialHashData SpatialHash;
+
+		[BurstCompile]
+		public void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity, ref ZombieSimulationAgentFOVResults fovResults,
+			in ZombieSimulationMovement movement, in LocalTransform localTransform)
+		{
+			fovResults.CanSeeZombie = false;
+			fovResults.CanSeeHealthyHuman = false;
+			fovResults.NearestVisibleHealthyEntity = Entity.Null;
 
 			float bestHealthyDistanceSq = float.MaxValue;
 			float bestZombieDistanceSq = float.MaxValue;
 
-			int2 minCellIndex = spatialHash.ValueRO.Comparer.ComputeCellIndex(localTransform.Position - settings.FOVDistance);
-			int2 maxCellIndex = spatialHash.ValueRO.Comparer.ComputeCellIndex(localTransform.Position + settings.FOVDistance);
+			int2 minCellIndex = SpatialHash.Comparer.ComputeCellIndex(localTransform.Position - Settings.FOVDistance);
+			int2 maxCellIndex = SpatialHash.Comparer.ComputeCellIndex(localTransform.Position + Settings.FOVDistance);
 
-			float radiusSq = settings.FOVDistance * settings.FOVDistance;
+			float radiusSq = Settings.FOVDistance * Settings.FOVDistance;
 			for (int y = minCellIndex.y; y <= maxCellIndex.y; ++y)
 			{
-				int minCellFlatIndex = spatialHash.ValueRO.Comparer.ComputeCellFlatIndex(new int2(minCellIndex.x, y));
+				int minCellFlatIndex = SpatialHash.Comparer.ComputeCellFlatIndex(new int2(minCellIndex.x, y));
 				int maxCellFlatIndex = minCellFlatIndex + 3;
-				int minIndex = spatialHash.ValueRO.CellStartIndices[math.min(minCellFlatIndex, spatialHash.ValueRO.MaxCellIndex)];
-				int maxIndex = spatialHash.ValueRO.CellStartIndices[math.min(maxCellFlatIndex, spatialHash.ValueRO.MaxCellIndex)];
+				int minIndex = SpatialHash.CellStartIndices[math.min(minCellFlatIndex, SpatialHash.MaxCellIndex)];
+				int maxIndex = SpatialHash.CellStartIndices[math.min(maxCellFlatIndex, SpatialHash.MaxCellIndex)];
 				for (int i = minIndex; i < maxIndex; ++i)
 				{
-					SpatialHashEntityData entityData = spatialHash.ValueRO.Entities[i];
+					SpatialHashEntityData entityData = SpatialHash.Entities[i];
 					float distance = math.distancesq(localTransform.Position, entityData.Position);
 					if (distance <= radiusSq)
 					{
 						float angleCos = math.dot(movement.Direction, math.normalize(entityData.Position - localTransform.Position));
-						if (angleCos < fovAngleCos) continue;
+						if (angleCos < FovAngleCos) continue;
 
 						float distanceSq = math.distancesq(localTransform.Position, entityData.Position);
 						if (distanceSq < bestHealthyDistanceSq && entityData.Type == SpatialHashEntityType.Human)
 						{
-							fovResults.ValueRW.CanSeeHealthyHuman = true;
-							fovResults.ValueRW.NearestVisibleHealthyEntity = entityData.Entity;
-							fovResults.ValueRW.NearestVisibleHealthyPosition = entityData.Position;
+							fovResults.CanSeeHealthyHuman = true;
+							fovResults.NearestVisibleHealthyEntity = entityData.Entity;
+							fovResults.NearestVisibleHealthyPosition = entityData.Position;
 							bestHealthyDistanceSq = distanceSq;
 						}
 
 						if (distanceSq < bestZombieDistanceSq && entityData.Type == SpatialHashEntityType.Zombie)
 						{
-							fovResults.ValueRW.CanSeeZombie = true;
-							fovResults.ValueRW.NearestVisibleZombiePosition = entityData.Position;
+							fovResults.CanSeeZombie = true;
+							fovResults.NearestVisibleZombiePosition = entityData.Position;
 							bestZombieDistanceSq = distanceSq;
 						}
 					}
 				}
 			}
 		}
-		m_Marker.End();
 	}
 }
